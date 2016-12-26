2ab7a9d7ae6af49304d03a43b0f50802c9b9024b
Tony <tonyeen@foxmail.com>
2016-12-27 02:16:41 +0800

    add nav

--- src/app/lib/layouts/layouts.module.ts
+++ src/app/lib/layouts/layouts.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FixedLayoutComponent } from './fixed-layout/fixed-layout.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [FixedLayoutComponent],
-  declarations: [FixedLayoutComponent, HeaderComponent]
+  declarations: [FixedLayoutComponent, HeaderComponent, SidebarComponent]
 })
 export class CuiLayoutsModule { }
