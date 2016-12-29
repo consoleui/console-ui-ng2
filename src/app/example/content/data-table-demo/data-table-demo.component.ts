eac12ec249d5ffb81b2f14da3398f1e7ed89aeb8
Yin Tao <yintao@parim.net>
2016-12-29 18:09:14 +0800

    table

--- src/app/example/content/data-table-demo/data-table-demo.component.ts
+++ src/app/example/content/data-table-demo/data-table-demo.component.ts
@@ -17,11 +17,20 @@ export class DataTableDemoComponent implements OnInit {
 
   ngOnInit() {
     this.columns = [
-      {title: 'ID'},
-      {title: 'TITLE'},
-      {title: 'Views'},
-      {title: 'Comments'},
+      {title: 'ID', prop: 'id'},
+      {title: 'TITLE', prop: 'title'},
+      {title: 'Views', prop: 'views'},
+      {title: 'Comments', prop: 'comments'},
       {title: 'Actions'}
+    ];
+
+    this.datas = [
+      {id: 10, title: '学习编程-先养成良好的习惯', views: 1878, comments: 1679},
+      {id: 20, title: '如何举一反三快速学习编程语言', views: 1878, comments: 1679},
+      {id: 30, title: '面向领域的问题解决办法', views: 1878, comments: 1679},
+      {id: 40, title: '语言不是万能的', views: 1878, comments: 1679},
+      {id: 50, title: '代码编写之道', views: 1878, comments: 1679},
+      {id: 60, title: '道法可以普及，但修成大道的却寥寥无几（论天赋的重要性）', views: 1878, comments: 1679},
     ];
 
     this.pagination = new PaginationModel(95, 10, 5);
--- src/app/lib/data-table/defs/api.ts
+++ src/app/lib/data-table/defs/api.ts
@@ -1,4 +1,5 @@
 
 export interface Column {
     title: string;
+    prop: string;
 }
